9954eff642d45c7752a37b06df5e8356f7013dc8
Sanket4550 <divatesanket3@gmail.com>
2026-08-08 01:15:48 +0530

    Update Assignment40_2.py

--- Assignment 40/Assignment40_2.py
+++ Assignment 40/Assignment40_2.py
@@ -11,10 +11,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 model = DecisionTreeClassifier()
 model.fit(X_train, y_train)
 
-df2 = df.drop('SleepHours', axis=1)
-X2 = df2.drop('FinalResult', axis=1)
-y2 = df2['FinalResult']
-X_train2, X_test2, y_train2, y_test2 = train_test_split(X2, y2, test_size=0.2, random_state=42)
-model2 = DecisionTreeClassifier()
-model2.fit(X_train2, y_train2)
-print("Accuracy without SleepHours:", accuracy_score(y_test2, model2.predict(X_test2)))
+acc = accuracy_score(y_test, model.predict(X_test))
+
+print("Accuracy without SleepHours:", acc)
+print("Answer: Compare with original accuracy to see impact of SleepHours")
